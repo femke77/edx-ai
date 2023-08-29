f960cc2f88b1bbf8eedd16ecf38ebff693b0d728
femke77 <megan.meyers.388@gmail.com>
2023-08-29 01:47:19 -0700

    first prompt with inquirer

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
-require('dotenv').config();
-console.log(process.env.OPENAI_API_KEY);
+// dependencies
+const { OpenAI } = require("langchain/llms/openai");
+require("dotenv").config();
+const inquirer = require('inquirer');
+
+// Creates and stores a wrapper for the OpenAI package along with basic configuration
+const model = new OpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0,
+  model: "gpt-3.5-turbo",
+});
+
+// console.log({ model });
+
+const init = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Ask a coding question:',
+    },
+  ]).then((inquirerResponse) => {
+    promptFunc(inquirerResponse.name)
+  });
+};
+
+const promptFunc = async (input) => {
+  try {
+    const res = await model.call(input);    
+    console.log(res);
+  }
+  catch (err) {
+    console.error(err);
+  }
+};
+
+
+init();
